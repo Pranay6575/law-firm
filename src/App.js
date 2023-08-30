446ac2045065852781ace4965e0695991506f724
Pranay <pranaypatil6575@gmail.com>
2023-08-30 18:47:01 +0530

    Our Team Section Completed

--- src/App.js
+++ src/App.js
@@ -2,12 +2,20 @@ import HeroSection from "./components/HeroSection/HeroSection";
 // import NavBar from "./components/Navbar/NavBar";
 import styles from "./App.module.css";
 import Introduction from "./components/Introduction/Introduction";
+import ChooseSection from "./components/ChooseSection/ChooseSection";
+import PracticeSection from "./components/PracticeSection/PracticeSection";
+import ClientSection from "./components/ClientSection/ClientSection";
+import Team from "./components/Team/Team";
 function App() {
   return (
     <>
       <HeroSection />
       <div className={styles.SectionWrapper}>
       <Introduction />
+      <ChooseSection />
+      <PracticeSection />
+      <ClientSection />
+      <Team />
       </div>
     </>
   );
--- src/components/HeroSection/HeroSection.jsx
+++ src/components/HeroSection/HeroSection.jsx
@@ -12,15 +12,15 @@ const HeroSection = () => {
         <div className={styles.HeadingText}>
           <p className={styles.TypoText1}>You Don't have to</p>
           <p className={styles.TypoText2}>Fight them Alone.</p>
-        <p className={styles.TypoPara}>
-          Lorem ipsum dolor sit amet consectetur adipiscing elit blandit,
-          curabitur sodales conubia ut inceptos faucibus himenaeos tortor eget,
-          hac massa gravida arcu interdum proin curae.
-        </p>
-        <EmailBar />
+          <p className={styles.TypoPara}>
+            Lorem ipsum dolor sit amet consectetur adipiscing elit blandit,
+            curabitur sodales conubia ut inceptos faucibus himenaeos tortor
+            eget, hac massa gravida arcu interdum proin curae.
+          </p>
+          <EmailBar />
         </div>
 
-        <div>
+        <div className={styles.ProfileImg}>
           <Profile />
         </div>
       </div>
